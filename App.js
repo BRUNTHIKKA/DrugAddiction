a64327d249c57782af71d286e1dbfa520a44825b
BRUNTHIKKA <122011243+BRUNTHIKKA@users.noreply.github.com>
2024-01-15 08:16:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import AuthStack from './navigation/AuthStack';
+import StudentHome from './navigation/screens/StudentHome';
+
+
+function App() {
+  return (
+    
+       <AuthStack />
+      
+    
+  );
+}
+
+export default App;
